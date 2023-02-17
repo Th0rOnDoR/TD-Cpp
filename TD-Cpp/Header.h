91aea55ec85d64b0be13558a518e00fdd2a3b709
Th0rOnDoR <thorondorytb@gmail.com>
2023-02-17 16:18:18 +0100

    LNK2005

--- TD-Cpp/Header.h
+++ TD-Cpp/Header.h
@@ -6,35 +6,37 @@ namespace TD
 {
 	class TD_Boucles {
 
-	public: static int Addition(int x, int y);
-	public: static int MultiplicationN(int x, int y);
-	public: static int MultiplicationZ(int x, int y);
-	public: static int Puissance(int x, int n);
-	public: static int Fibonacci(int n);
-	public: static int Mirroir(int x);
-	public: static int Quotient(int x, int y);
-	public: static int Factorielle(int x);
-	public: static int FactorielleCalculbre(int limit);
+	public: 
+		static int Addition(int x, int y);
+		static int MultiplicationN(int x, int y);
+		static int MultiplicationZ(int x, int y);
+		static int Puissance(int x, int n);
+		static int Fibonacci(int n);
+		static int Mirroir(int x);
+		static int Quotient(int x, int y);
+		static int Factorielle(int x);
+		static int FactorielleCalculbre(int limit);
 	};
 
 
 	class TD_Strings {
-	public: static int Recherche(string str);
-	public: static int Appartient(string str, char c);
-	public: static bool Palindrome(string str);
-	public: static bool EstPhrase(string str);
-	public: static int Sous_chaine(string str, string sub_str);
-	public: static int Sous_palindrome(string str);
+	public: 
+		static int Recherche(string str);
+		static int Appartient(string str, char c);
+		static bool Palindrome(string str);
+		static bool EstPhrase(string str);
+		static int Sous_chaine(string str, string sub_str);
+		static int Sous_palindrome(string str);
 	};
 
 
 	class TD_Imperative {
-
-	public: static bool test(int n);
-	public: static int sum_digits(int n);
-	public: static int product_digit(int n);
-	public: static int abs(int n);
-	public: static int loop(int n);
-	public: static int List_to_9(int x);
+	public: 
+		static bool test(int n);
+		static int sum_digits(int n);
+		static int product_digit(int n);
+		static int abs(int n);
+		static int loop(int n);
+		static int List_to_9(int x);
 	};
 }
--- TD-Cpp/Strings.cpp
+++ TD-Cpp/Strings.cpp
@@ -1,46 +1,43 @@
 #include <iostream>
 #include <math.h>
 #include <string>
-
 #include "Header.h"
-using namespace TD;
+
 using std::string;
 
+namespace TD {
 
-
-	class TD_Strings {
-
-	public: static int Recherche(string str) {
+	int TD_Strings::Recherche(string str) {
 		throw std::exception(); // à supprimer pour faire la fonction
 		//Ecrivez votre réponse ici ^^
 	}
 
-	public: static int Appartient(string str, char c) {
+	int TD_Strings::Appartient(string str, char c) {
 		throw std::exception(); // à supprimer pour faire la fonction
 
 		//Ecrivez votre réponse ici ^^
 	}
 
-	public: static bool Palindrome(string str) {
+	bool TD_Strings::Palindrome(string str) {
 		throw std::exception(); // à supprimer pour faire la fonction
 
 		//Ecrivez votre réponse ici ^^
 	}
 
-	public: static bool EstPhrase(string str) {
+	bool TD_Strings::EstPhrase(string str) {
 		throw std::exception(); // à supprimer pour faire la fonction
 
 		//Ecrivez votre réponse ici ^^
 	}
 
 
-	public: static int Sous_chaine(string str, string sub_str) {
+	int TD_Strings::Sous_chaine(string str, string sub_str) {
 		throw std::exception(); // à supprimer pour faire la fonction
 
 		//Ecrivez votre réponse ici ^^
 	}
 
-	public: static int Sous_palindrome(string str) {
+	int TD_Strings::Sous_palindrome(string str) {
 		throw std::exception(); // à supprimer pour faire la fonction
 
 		//Ecrivez votre réponse ici ^^
